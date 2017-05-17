5885cc39a42b841a219c5e0cb4b27c66148376d6
jcarlos79t <jcarlos79t@hotmail.com>
2017-05-17 15:22:52 -0400

    Subir git
    Subiendo a githib

---
+++ BusEnvia/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+
+namespace BusEnvia
+{
+    [Activity(Label = "BusEnvia", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+           SetContentView (Resource.Layout.Main);
+
+
+          
+        }
+    }
+}
+
